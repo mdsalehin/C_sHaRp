0982443ebb2eb7c6c11e2b69f84a1592f6a93ff4
mdsalehin <salahin1999@gmail.com>
2024-02-21 04:09:05 +0600

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+    
+class Bio{
+    public static void Main( string[] arg)
+    {
+        Console.WriteLine("MY Name is Md Salahin");
+        Console.WriteLine("Now I am learning C# Programming");
+        Console.WriteLine("I am passionate for best backend Developer");
+    
+    }
+
+}
