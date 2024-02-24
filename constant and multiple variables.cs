20dc68829ebc35234bc21805fd8696fbbcfb1128
mdsalehin <salahin1999@gmail.com>
2024-02-24 17:22:42 +0600

    constant and multiple variables

--- constant and multiple variables.cs
+++ constant and multiple variables.cs
@@ -1,24 +1,39 @@
-
-class Variable{
-    public static void Main(string[] arg)
+using System;
+class Constant
+{
+    public static void Main(string[] args)
     {
-        Console.WriteLine("This is For Variables");
+        string University = "Bangladesh University Of Business And Technology";
+        Console.WriteLine(University);
 
-        string var;
-        var = "Hello i am Md Salehin.";
-        
-        Console.WriteLine(var);
-        Console.WriteLine(var);
-        Console.WriteLine(var);
+        Console.WriteLine("\n");
 
-        var = "Changing any time varibale like thiss.-->>Now my name is MS7";
+        Console.WriteLine("Now I want to chage My University Name");
+        University = "Mirpur Bangla school\n";
+        Console.WriteLine(University);
 
-        Console.WriteLine(var);
-        Console.WriteLine(var);
-        Console.WriteLine(var);
+        Console.WriteLine("Const Variable\n");
+
+        const string IdontWantTocHange = "This is i don't want to change so this is the power of const variable\n";
+        Console.WriteLine(IdontWantTocHange);
 
 
-        Console.WriteLine("Now Using Camel Case");
-        string CamelCase;
-        CamelCase = "CamelCASE using Max Leangth OF word of VARIABLE";
-        Console.WriteLine(CamelCase);
+
+         Console.WriteLine("Now for Multiple  Variable\n");
+
+                int num1,num2,num3;
+                num1=10;
+                num2=10;
+                num3=10;
+                Console.WriteLine(num1);
+                Console.WriteLine(num2);
+                Console.WriteLine(num3);
+
+        Console.WriteLine("another Process of declare the same value");
+                num1=num2=num3=10;
+                Console.WriteLine(num1);
+                Console.WriteLine(num2);
+                Console.WriteLine(num3);
+
+    }
+}
