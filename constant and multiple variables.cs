8c4dd951fd2e59821e15ddb0af1d422fcf445c8b
mdsalehin <salahin1999@gmail.com>
2024-02-24 17:19:11 +0600

    so this it

---
+++ constant and multiple variables.cs
@@ -0,0 +1,24 @@
+
+class Variable{
+    public static void Main(string[] arg)
+    {
+        Console.WriteLine("This is For Variables");
+
+        string var;
+        var = "Hello i am Md Salehin.";
+        
+        Console.WriteLine(var);
+        Console.WriteLine(var);
+        Console.WriteLine(var);
+
+        var = "Changing any time varibale like thiss.-->>Now my name is MS7";
+
+        Console.WriteLine(var);
+        Console.WriteLine(var);
+        Console.WriteLine(var);
+
+
+        Console.WriteLine("Now Using Camel Case");
+        string CamelCase;
+        CamelCase = "CamelCASE using Max Leangth OF word of VARIABLE";
+        Console.WriteLine(CamelCase);
