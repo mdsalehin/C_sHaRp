d6735ebc13a2d45654a3ed65da5eb5c2514fef3b
mdsalehin <salahin1999@gmail.com>
2024-02-24 16:21:26 +0600

    Assignment task Anisul

--- Program.cs
+++ Program.cs
@@ -98,27 +98,6 @@ class Variable{
 }
 */
 
-// Assignment - 2
-
-using System;
-
-class Assignment{
-    public static void Main(string[] arg)
-    {
-        string Name = "Apple Iphone";
-        double price = 320.5;
-        string category = "smart phone";
-        bool available = true;
-        int sold = 5;
-
-        Console.WriteLine(Name);
-        Console.WriteLine(price);
-        Console.WriteLine($"${price}"); 
-        Console.WriteLine(category);
-        Console.WriteLine(available);
-        Console.WriteLine(sold);
-    }
-}
 
 
 
---
+++ assignment.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Assignment{
+    public static void Main(string[] arg)
+    {
+        string Name = "Apple Iphone";
+        double price = 320.5;
+        string category = "smart phone";
+        bool available = true;
+        int sold = 5;
+
+        Console.WriteLine(Name);
+        Console.WriteLine(price);
+        Console.WriteLine($"${price}"); 
+        Console.WriteLine(category);
+        Console.WriteLine(available);
+        Console.WriteLine(sold);
+    }
+}
